f3314c75f2161342a37e62570d958ea39dab5291
Natalia Chernysheva <chernyshevanata374@gmail.com>
2026-05-17 16:16:24 +0000

    fix: database updated to DBT_NCHERNYSHEVA

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,11 +1,9 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
-    {%- set default_schema = target.schema -%}
-
     {%- if custom_schema_name is none -%}
-        {{ default_schema | trim }}
+        {{ target.schema | trim }}
     {%- else -%}
-        {{ default_schema | trim }}_{{ custom_schema_name | trim | upper }}
+        {{ custom_schema_name | trim }}
     {%- endif -%}
 
 {%- endmacro %}
