684e6892d5d2f2f3206f7ca62bfb3d93171120c7
Natalia Chernysheva <chernyshevanata374@gmail.com>
2026-05-18 08:55:13 +0000

    fix: COALESCE en stg_libro.id_editorial

--- models/staging/stg_libro.sql
+++ models/staging/stg_libro.sql
@@ -11,8 +11,8 @@ renamed as (
         LOWER(categoria)        as categoria,
         precio::FLOAT           as precio
     from source
-    where id_libro is not null -- solo este filtro, quitamos precio/autor(añadimos editorial, ya que nos han entrado los libros sin editorial. Not null en editorial les detecta)
-      and id_editorial  is not null
+    where id_libro is not null -- solo este filtro, quitamos precio/autor
+    COALESCE(UPPER(id_editorial), 'SIN_EDITORIAL') as id_editorial
 ),
 
 deduplicado as (
