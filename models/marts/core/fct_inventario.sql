6af65ad650161125c4cba66572dec182a16735e5
Natalia Chernysheva <chernyshevanata374@gmail.com>
2026-05-19 09:49:34 +0000

    new fct_inventario

--- models/marts/core/fct_inventario.sql
+++ models/marts/core/fct_inventario.sql
@@ -1,32 +1,41 @@
-{{ config(materialized='table') }}
+{{
+    config(
+        materialized='incremental',
+        unique_key='id_stock',
+        incremental_strategy='merge'
+    )
+}}
 
 with stock as (
     select * from {{ ref('stg_stock') }}
+
+    {% if is_incremental() %}
+        where fecha_actualizacion > (select max(fecha_actualizacion) from {{ this }})
+    {% endif %}
 ),
 
 dim_libro as (
-    select sk_libro, id_libro, titulo
+    select sk_libro, id_libro, id_editorial
     from {{ ref('dim_libro') }}
 ),
 
-dim_libreria as (
-    select sk_libreria, id_libreria, nombre_libreria
-    from {{ ref('dim_libreria') }}
+dim_editorial as (
+    select sk_editorial, id_editorial
+    from {{ ref('dim_editorial') }}
 )
 
 select
     s.id_stock,
     dl.sk_libro,
-    dlb.sk_libreria,
+    de.sk_editorial,
     s.stock_inicial,
     s.unidades_vendidas,
     s.stock_actual,
     s.necesita_reposicion,
     s.fecha_actualizacion,
     dl.id_libro,
-    dl.titulo,
-    dlb.id_libreria,
-    dlb.nombre_libreria
+    de.id_editorial
 from stock s
-left join dim_libro    dl  on s.id_libro    = dl.id_libro
-left join dim_libreria dlb on s.id_libreria = dlb.id_libreria
+left join dim_libro     dl on s.id_libro      = dl.id_libro
+left join dim_editorial de on dl.id_editorial = de.id_editorial
+where s.stock_actual < 5
