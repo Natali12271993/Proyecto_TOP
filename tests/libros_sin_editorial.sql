0ba04718069ec251e73079e8b64f1b373e4e5945
Natalia Chernysheva <chernyshevanata374@gmail.com>
2026-05-19 11:00:44 +0000

    singular test: un email por cliente

--- tests/libros_sin_editorial.sql
+++ tests/libros_sin_editorial.sql
@@ -2,6 +2,6 @@
 -- Si aparece un libro nuevo sin editorial el test falla.
 
 select id_libro
-from {{ ref('stg_libros') }}
-where id_editorial is null
+from {{ ref('stg_libro') }}
+where id_editorial = 'SIN_EDITORIAL'
   and id_libro not in ('L0436', 'L0766', 'L0795', 'LM0398', 'LM0685', 'LM0765')
